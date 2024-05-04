a82c294a12b7f0c2ff776c0d81f424741e670cd3
IgnacioGoldchluk <goldchlukignacio@gmail.com>
2024-05-04 15:10:15 -0300

    custom status code

--- lib/stub_server/routes.ex
+++ lib/stub_server/routes.ex
@@ -5,10 +5,11 @@ defmodule StubServer.Routes do
     quote bind_quoted: [endpoint_info: endpoint_info] do
       %{"route" => route, "response" => resp} = endpoint_info
       method = Map.get(endpoint_info, "method", "get") |> String.to_atom()
+      status_code = Map.get(endpoint_info, "status_code", 200)
 
       match route, via: method do
         var!(conn)
-        |> send_resp(200, "hello")
+        |> send_resp(unquote(status_code), "hello")
       end
     end
   end
